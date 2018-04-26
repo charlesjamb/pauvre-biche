f9a6dea0c3c420f1c54db831a270739a7d6c0d1a
Charles <charles.jambon@gmail.com>
2018-04-26 18:40:18 +0200

    Create landing page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,7 +6,7 @@ import Header from '../components/header'
 import './index.css'
 
 const Layout = ({ children, data }) => (
-  <div>
+  <div style={{'height': '100%'}}>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
@@ -14,13 +14,13 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
+    {
+      // <Header siteTitle={data.site.siteMetadata.title} />
+    }
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        'width': '100%',
+        'height': '100%',
       }}
     >
       {children()}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className='landing-page'>
+    <h1 className='coming'>c'est pour bientôt ...</h1>
+    {
+      // <Link to="/page-2/">Go to page 2</Link>
+    }
   </div>
 )
 
